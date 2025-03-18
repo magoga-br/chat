26ff3a6a15a383157807b6e236a5e671ddbfa8b0
fabricio magoga <falolmagoga@gmail.com>
2025-03-18 03:17:36 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -93,7 +93,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-634v.onrender.com")
     websocket.onmessage = processMessage
 
 }
